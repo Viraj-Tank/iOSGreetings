3e2dae807c6aebb41f246cb03c873adadeecfaef
VirajTank <virajtank98@gmail.com>
2022-12-25 22:18:39 +0530

    text modifiers

--- iOSGreetings/ContentView.swift
+++ iOSGreetings/ContentView.swift
@@ -2,11 +2,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            
+            Text("Greetings!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color.green.opacity(0.5))
+                .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+                .shadow(color: Color.green.opacity(0.5), radius: 5,x: 5,y: 10)
+            
+            Text("Welcome to Swift programming")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color.gray.opacity(0.5))
+                .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+                .shadow(color: Color.gray.opacity(0.5), radius: 5,x: 5,y: 10)
+            
+            Text("Are you ready to..")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color.yellow)
+                .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+                .shadow(color: Color.yellow.opacity(0.5), radius: 5,x: 5,y: 10)
+            
+            Text("start exploring?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color.red.opacity(0.5))
+                .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+                .shadow(color: Color.red.opacity(0.5), radius: 5,x: 5,y: 10)
+            
+            Text("Boom!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color.purple.opacity(0.5))
+                .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+                .shadow(color: Color.purple.opacity(0.9), radius: 5,x: 5,y: 10)
         }
         .padding()
     }

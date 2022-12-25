bb25a8432c5aa3bed04d983f902d7866114828a6
VirajTank <virajtank98@gmail.com>
2022-12-25 19:39:15 +0530

    Initial Commit

---
+++ iOSGreetings/iOSGreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSGreetingsApp.swift
+//  iOSGreetings
+//
+//  Created by Viraj Tank on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSGreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

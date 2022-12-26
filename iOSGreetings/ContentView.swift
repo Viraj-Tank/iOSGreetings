8478ea2b2b5673eabec49a51141955f69a36011f
VirajTank <virajtank98@gmail.com>
2022-12-26 21:12:36 +0530

    components with stacks and loops

--- iOSGreetings/ContentView.swift
+++ iOSGreetings/ContentView.swift
@@ -1,55 +1,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let messages = [
+        DataItemModel(name: "Greetings!", color: .green),
+        DataItemModel(name: "Welcome to Swift programming", color: .gray),
+        DataItemModel(name: "Are you ready to..", color: .yellow),
+        DataItemModel(name: "start exploring?", color: .red),
+        DataItemModel(name: "Boom!", color: .purple)
+    ]
+    
     var body: some View {
         VStack(alignment: .leading) {
             
-            Text("Greetings!")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .foregroundColor(Color.white)
-                .padding()
-                .background(Color.green.opacity(0.5))
-                .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
-                .shadow(color: Color.green.opacity(0.5), radius: 5,x: 5,y: 10)
+            VStack(alignment: .leading,spacing: 4,content: {
+                Text("Greetings!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("Exploring iOS 16 programming")
+                    .font(.headline)
+                    .fontWeight(.light)
+            })
             
-            Text("Welcome to Swift programming")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .foregroundColor(Color.white)
-                .padding()
-                .background(Color.gray.opacity(0.5))
-                .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
-                .shadow(color: Color.gray.opacity(0.5), radius: 5,x: 5,y: 10)
             
-            Text("Are you ready to..")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .foregroundColor(Color.white)
-                .padding()
-                .background(Color.yellow)
-                .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
-                .shadow(color: Color.yellow.opacity(0.5), radius: 5,x: 5,y: 10)
-            
-            Text("start exploring?")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .foregroundColor(Color.white)
-                .padding()
-                .background(Color.red.opacity(0.5))
-                .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
-                .shadow(color: Color.red.opacity(0.5), radius: 5,x: 5,y: 10)
-            
-            Text("Boom!")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .foregroundColor(Color.white)
-                .padding()
-                .background(Color.purple.opacity(0.5))
-                .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
-                .shadow(color: Color.purple.opacity(0.9), radius: 5,x: 5,y: 10)
+            ForEach(messages, content: { msg in
+                TextView(text: msg.name, color: msg.color)
+            })
+            /*ForEach(0..<messages.count, content: { index in
+                TextView(text: messages[index].name, color: messages[index].color)
+            })*/
         }
-        .padding()
     }
 }
 
@@ -57,4 +37,27 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct TextView: View {
+    let text:String
+    let color:Color
+    var body: some View {
+        Text(text)
+            .font(.title)
+            .fontWeight(.semibold)
+            .foregroundColor(Color.white)
+            .padding()
+            .background(color.opacity(0.5))
+            .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+            .shadow(color: color.opacity(0.5), radius: 5,x: 5,y: 10)
+    }
+}
+
+
+struct DataItemModel: Identifiable {
+    var id = UUID()
+    
+    let name: String
+    let color: Color
 }
